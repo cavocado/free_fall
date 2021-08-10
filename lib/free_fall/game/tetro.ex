97f09326c751ba5c8935c3c1f325e39d0cede6f2
Alexa Hunleth <cavocado@users.noreply.github.com>
2021-08-10 09:37:15 -0400

    Added a converter to tetro.ex

--- lib/free_fall/game/tetro.ex
+++ lib/free_fall/game/tetro.ex
@@ -1,21 +1,27 @@
 defmodule Tetro do
-  defstruct [shape: :l, position: {5,1}, rotation: 0] 
+  defstruct shape: :l, position: {5, 1}, rotation: 0
 
-  def new do 
+  def new do
     %__MODULE__{}
   end
-  
+
   def new(shape) do
-    %__MODULE__{shape: shape}    
+    %__MODULE__{shape: shape}
   end
 
   def down(tetro) do
-    %{tetro| position: Point.down(tetro.position)}
+    %{tetro | position: Point.down(tetro.position)}
   end
+
   def left(tetro) do
-    %{tetro| position: Point.left(tetro.position)}
+    %{tetro | position: Point.left(tetro.position)}
   end
+
   def right(tetro) do
-    %{tetro| position: Point.right(tetro.position)}
+    %{tetro | position: Point.right(tetro.position)}
+  end
+
+  def convert(tetro) do
+    {tetro.shape, tetro.rotation, tetro.position}
   end
 end
